a687e9faf92edaaeb5d7e543bce8f055d09222a8
ytop <tomilovyury@mail.ru>
2024-01-03 16:02:43 +0300

    AuthContent WIP

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,4 +42,5 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
     implementation("androidx.navigation:navigation-compose:2.7.6")
+    implementation("androidx.compose.material:material-icons-extended:1.5.4")
 }
